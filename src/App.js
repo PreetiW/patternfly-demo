c7d7036e0c66f0a080afc87c5db4a627c95d6201
Preeti Wadhwani <pwadhwani2592@gmail.com>
2023-08-10 15:59:23 +0530

    using basic card and button component from patternfly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {Card, CardTitle, CardBody, CardFooter, Button} from '@patternfly/react-core';
+import '@patternfly/react-core/dist/styles/base.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <Card style={{ margin: '16px'}} ouiaId="BasicCard">
+          <CardTitle>Preeti Wadhwani</CardTitle>
+          <CardBody>Senior Softwar Engineer</CardBody>
+          <CardFooter>Red Hat</CardFooter>
+        </Card>
+
+        <Button variant="primary" ouiaId="Primary">Click Me!!</Button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>Hello World!</div>
         <a
           className="App-link"
           href="https://reactjs.org"
